2f1158d7dccee711259d1cc2b62eb44289165586
romncn <rom.ncn@gmail.com>
2020-10-24 02:28:08 +0700

    💄update ui & create form

--- src/index.js
+++ src/index.js
@@ -10,9 +10,17 @@ body {
     padding: 0;
     font-family: 'Mitr', sans-serif;
     font-weight: 500;
-    font-size: 18px;
   }
-`
+  .ant-select-single:not(.ant-select-customize-input) .ant-select-selector {
+        border-radius: 4px;
+        height: 40px;
+        font-size: 16px;
+        align-items: center;
+        &:hover , &:active, &:focus {
+            border-color: rgb(230, 111, 15);
+        }
+    }
+`;
 
 ReactDOM.render(
   <>
